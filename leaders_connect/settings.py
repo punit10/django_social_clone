5ac6cd380d03d92526dbf3743d52e47b6e6ab16b
Punit Sharma <punitcs0067@gmail.com>
2024-06-11 18:46:21 +0530

    Signup, Login and logout Done!!

--- leaders_connect/settings.py
+++ leaders_connect/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'accounts'
 ]
 
@@ -120,6 +121,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
