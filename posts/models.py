bb3f75b1d9aecc23b5d00bdb41eb24395b60a194
Punit Sharma <punitcs0067@gmail.com>
2024-06-14 11:54:32 +0530

    Added models for groups and posts, Done!

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
+from django.core import settings
+import misaka
 
+from group.models import Group
 # Create your models here.
+# POSTS Model.py
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group, related_name='posts', null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.message
+
+    def save(self, *args, **kwargs):
+        self.message_html = misaka.html(self.message)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('post:single', kwargs={'username': self.user.username,
+                       'pk': self.pk})
+
+    def Meta:
+        ordering = ['-created_at']
+        unique_together = ['user', 'message']
+
+
+
+
